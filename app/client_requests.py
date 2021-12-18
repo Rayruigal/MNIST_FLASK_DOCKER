24fdaf9b0350072d16cfbeab237dedf193ff91a9
rui <ruihu.xidian@gmail.com>
2021-12-18 15:47:00 +0100

    edit the Requests based function to call the Flask api

--- app/client_requests.py
+++ app/client_requests.py
@@ -1,9 +1,18 @@
 import requests
-#import json
+import argparse
 
-url = 'http://127.0.0.1:5000/'
-input = {'dir': 'resources/images/mnist/mnist_test_0.png'}
 
-response = requests.post(url, json = input)
-result = response.json()['label']
-print('The test image is of number: ' + result)
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Example of parser. ')
+    parser.add_argument('--url', type=str, default='http://localhost:5000/', help = 'url of the Flask server')
+    parser.add_argument('--dir', type=str, default='resources/images/mnist/mnist_test_0.png', help = 'local directory of your test image')
+    args = parser.parse_args()
+
+    url = args.url
+    input = {}
+    input['dir'] = args.dir
+
+    response = requests.post(url, json=input)
+    result = response.json()['label']
+    print('The test image is of number: ' + result)
