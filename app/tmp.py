aea335cc61c524c06524df965f2b8e4921368944
Rayruigal <ruihu.xidian@gmail.com>
2021-12-18 16:15:51 +0100

    delete unused code

--- app/tmp.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify, request
-import requests
-r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-r.status_code
-r.headers['content-type']
-r.encoding
-r.text
-r.json()
